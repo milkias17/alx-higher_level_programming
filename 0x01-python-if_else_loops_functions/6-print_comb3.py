e639833ee6c57b0ecca64642f7b7b1c37bb63b4c
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2022-05-31 22:56:47 +0300

    Do task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 
-for num in range(1, 100):
-    pass
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
