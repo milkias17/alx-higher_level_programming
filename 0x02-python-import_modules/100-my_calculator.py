0e1c08370f71c6a68862b63b2a503c738d6c5edc
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2022-06-04 22:29:10 +0300

    Do more assignments!!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,27 +3,21 @@ from sys import argv, exit
 
 from calculator_1 import add, div, mul, sub
 
-arg_len = len(argv)
 if __name__ == "__main__":
-    if arg_len < 4:
+    arg_len = len(argv)
+    if arg_len != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     operator = argv[2]
+    a = int(argv[1])
+    b = int(argv[3])
     if operator == "+":
-        a = int(argv[1])
-        b = int(argv[3])
         print(f"{a} + {b} = {add(a, b)}")
     elif operator == "-":
-        a = int(argv[1])
-        b = int(argv[3])
         print(f"{a} - {b} = {sub(a, b)}")
     elif operator == "*":
-        a = int(argv[1])
-        b = int(argv[3])
         print(f"{a} * {b} = {mul(a, b)}")
     elif operator == "/":
-        a = int(argv[1])
-        b = int(argv[3])
         print(f"{a} * {b} = {div(a, b)}")
     else:
         print("Unknown operator. Available operators: +, -, * and /")
