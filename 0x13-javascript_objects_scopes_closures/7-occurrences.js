07534bfc4b26937f63e54e0595bd7f744ec161a3
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2022-09-07 12:27:21 +0300

    do javascript assignment on classes and closures

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+function nbOccurences(list, searchElement) {
+  let count = 0;
+  list.forEach(el => {
+    if (el === searchElement)
+      count++;
+  })
+
+  return count;
+}
+
+module.exports = {nbOccurences};
